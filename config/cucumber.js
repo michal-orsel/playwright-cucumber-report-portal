91ecbc4d685610266cac6c981a2cdfbeec35913e
Michal Orsel <michal@orsel.cz>
2024-05-27 21:52:03 +0200

    v 1.1 test run

--- config/cucumber.js
+++ config/cucumber.js
@@ -1,55 +1,68 @@
 const path = require("path")
 const config = require('./config.json');
 
+let configTemplate = {
+    tags: process.env.npm_config_TAGS || "",
+    parallel: 2,
+    formatOptions: {
+        snippetInterface: "async-await"
+    },
+    paths: [
+        "features/features"
+    ],
+    publishQuite: true,
+    dryRun: false,
+    require: [
+        "features/*.ts",
+        "features/**/*.ts"
+    ],
+    requireModule: [
+        "ts-node/register"
+    ],
+    format: [
+        "./reportportal_formatter.js",
+        ['html', `${path.join(config.pathToReports, "cucumber_report.html")}`],
+        ['json', `${path.join(config.pathToReports, "cucumber_report.json")}`],
+        "rerun:@rerun.txt"
+    ],
+    retry: 1
+};
+
 module.exports = {
     default: {
-        tags: process.env.npm_config_TAGS || "",
-        parallel: 1,
-        formatOptions: {
-            snippetInterface: "async-await"
-        },
-        paths: [
-            "features/features"
-        ],
-        publishQuite: true,
-        dryRun: false,
-        require: [
-            "features/*.ts",
-            "features/**/*.ts"
-        ],
-        requireModule: [
-            "ts-node/register"
-        ],
+        ...configTemplate
+    },
+    rerun: {
+        ...configTemplate,
+        paths: [],
         format: [
-            "./reportportal_formatter.js",
-            // "progress-bar",
+            "./reportportal_formatter_rerun.js",
+            ['html', `${path.join(config.pathToReports, "cucumber_report_rerun.html")}`],
+            ['json', `${path.join(config.pathToReports, "cucumber_report_rerun.json")}`],
+            "rerun:@rerun.txt"
+        ],
+        retry: 0
+    },
+    launch_file: {
+        ...configTemplate,
+        parallel: 4,
+        paths: [],
+        retry: 0
+    },
+    launch_file_no_reportportal: {
+        parallel: 4,
+        paths: [],
+        format: [
+            "summary",
             ['html', `${path.join(config.pathToReports, "cucumber_report.html")}`],
             ['json', `${path.join(config.pathToReports, "cucumber_report.json")}`],
             "rerun:@rerun.txt"
         ],
-        retry: 1
+        retry: 0
     },
-    rerun: {
+    cucumber_js_test_runner: {
         parallel: 4,
-        formatOptions: {
-            snippetInterface: "async-await"
-        },
-        publishQuite: true,
-        dryRun: false,
-        require: [
-            "features/*.ts",
-            "features/**/*.ts"
-        ],
-        requireModule: [
-            "ts-node/register"
-        ],
-        format: [
-            "./reportportal_formatter_rerun.js",
-            // "progress-bar",
-            ['html', `${path.join(config.pathToReports, "cucumber_report_rerun.html")}`],
-            ['json', `${path.join(config.pathToReports, "cucumber_report_rerun.json")}`],
-            "rerun:@rerun2.txt"
-        ],
-        retry: 1
+        paths: [],
+        retry: 0
     }
 }
